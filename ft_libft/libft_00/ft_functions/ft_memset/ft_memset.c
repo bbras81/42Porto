0cd6b714636c5f331036fc25291d521e4a5697b7
Bruno Brás <miguelazevedodg@gmail.com>
2025-04-07 14:44:44 +0100

    formmater solved

--- ft_libft/libft_00/ft_functions/ft_memset/ft_memset.c
+++ ft_libft/libft_00/ft_functions/ft_memset/ft_memset.c
@@ -3,23 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brunmigu <brunmigu@students.42porto.com>   +#+  +:+       +#+        */
+/*   By: brunmigu <brunmigu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 10:10:52 by brunmigu          #+#    #+#             */
-/*   Updated: 2025/03/26 10:20:36 by brunmigu         ###   ########.fr       */
+/*   Updated: 2025/04/07 13:38:17 by brunmigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
-int *ft_memset() { return (0); }
 
-int main() {
-  char *test;
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t	counter;
 
-  test = "Ola Mundo";
-  printf("Antes: %s", test);
-  memset(&test, 0, sizeof(test));
-  printf("Depois: %s", test);
-  return (0);
+	counter = 0;
+	while (counter != n)
+	{
+		s = c;
+		s++;
+	}
+}
+
+int	main(void)
+{
+	char	test[5];
+
+	printf("Antes: %s", test);
+	ft_memset(&test, 'a', 3);
+	printf("Depois: %s", test);
+	return (0);
 }
