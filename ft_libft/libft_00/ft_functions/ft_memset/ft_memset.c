7476397caf9eb062d37cee78b5c2a28b092daefe
Bruno Brás <miguelazevedodg@gmail.com>
2025-04-08 11:57:07 +0100

    funcions

--- ft_libft/libft_00/ft_functions/ft_memset/ft_memset.c
+++ ft_libft/libft_00/ft_functions/ft_memset/ft_memset.c
@@ -10,28 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
-#include <stdio.h>
-#include <string.h>
+#include "libft.h"
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	size_t	counter;
+	unsigned char	*ptr;
+	size_t			counter;
 
+	ptr = (unsigned char *)s;
 	counter = 0;
 	while (counter != n)
 	{
-		s = c;
-		s++;
+		ptr[counter] = (unsigned char)c;
+		counter++;
 	}
-}
-
-int	main(void)
-{
-	char	test[5];
-
-	printf("Antes: %s", test);
-	ft_memset(&test, 'a', 3);
-	printf("Depois: %s", test);
-	return (0);
+	return (ptr);
 }
